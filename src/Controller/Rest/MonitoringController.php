16d957242ec59680e530a0ad3972cf13c7af3533
cHeeSaW <cheesaw@gmx.net>
2018-11-15 23:59:54 +0100

    add sandbox documentation and empty response

--- src/Controller/Rest/MonitoringController.php
+++ src/Controller/Rest/MonitoringController.php
@@ -5,8 +5,12 @@ declare(strict_types=1);
 namespace App\Controller\Rest;
 
 use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Swagger\Annotations as SWG;
+use App\Dto\MonitoringData;
 
 /**
  * @Route("/api")
@@ -17,13 +21,22 @@ class MonitoringController extends FOSRestController
      * @Route("/monitoring/data", methods={"POST"})
      * @SWG\Response(
      *     response=201,
-     *     description="Expects the data that will be posted to the dashboard",
+     *     description="Expects the data that will be posted to the dashboard"
+     * )
+     *
+     * @SWG\Parameter(
+     *     name="body",
+     *     in="body",
+     *     required=true,
+     *     allowEmptyValue=false,
+     *     @SWG\Schema(ref=@Model(type=MonitoringData::class))
+     *
      * )
      *
      * @SWG\Tag(name="Monitoring")
      */
-    public function postMonitoringData()
+    public function postMonitoringData(): JsonResponse
     {
-
+        return new JsonResponse(null, Response::HTTP_CREATED);
     }
 }
