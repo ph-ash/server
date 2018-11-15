f375e1d967d019ec34c4ec9fe7bba8d4ed91c006
cHeeSaW <cheesaw@gmx.net>
2018-11-15 23:15:14 +0100

    configure nelmio, add twig and symfony asset

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
