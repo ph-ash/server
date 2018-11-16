019dd9fabef5e61083621142fb84a52ebe9f4275
cHeeSaW <cheesaw@gmx.net>
2018-11-16 08:17:53 +0100

    add service for dispatching incoming data event

---
+++ src/Service/IncomingMonitoringDataDispatcher.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Service;
+
+use App\Dto\MonitoringData;
+
+interface IncomingMonitoringDataDispatcher
+{
+    public function invoke(MonitoringData $monitoringData);
+}
---
+++ src/Service/IncomingMonitoringDataDispatcherService.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service;
+
+use App\Dto\MonitoringData;
+use App\Event\IncomingMonitoringDataEvent as Event;
+use App\Factory\IncomingMonitoringDataEvent;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class IncomingMonitoringDataDispatcherService implements IncomingMonitoringDataDispatcher
+{
+    private $eventDispatcher;
+    private $incomingMonitoringDataEvent;
+
+    public function __construct(EventDispatcherInterface $eventDispatcher, IncomingMonitoringDataEvent $incomingMonitoringDataEvent)
+    {
+        $this->eventDispatcher = $eventDispatcher;
+        $this->incomingMonitoringDataEvent = $incomingMonitoringDataEvent;
+    }
+
+    public function invoke(MonitoringData $monitoringData)
+    {
+        $event = $this->incomingMonitoringDataEvent->create($monitoringData);
+        $this->eventDispatcher->dispatch(Event::EVENT_INCOMING_MONITORING_DATA, $event);
+    }
+}
