bdff2523f35a5220493a8d574ad930c8fbd29501
cHeeSaW <cheesaw@gmx.net>
2018-11-17 18:44:59 +0100

    use zmq client to push data

--- src/Service/Board/MonitoringDataPushService.php
+++ src/Service/Board/MonitoringDataPushService.php
@@ -4,10 +4,23 @@ declare(strict_types=1);
 
 namespace App\Service\Board;
 
+use App\Dto\MonitoringData;
+use App\Service\Board\ZMQ\PushClient;
+
 class MonitoringDataPushService implements MonitoringDataPush
 {
-    public function invoke()
-    {
+    private $pushClient;
 
+    public function __construct(PushClient $pushClient)
+    {
+        $this->pushClient = $pushClient;
+    }
+
+    public function invoke(MonitoringData $monitoringData)
+    {
+        //TODO test
+        //TODO exception handling
+        $this->pushClient->send($monitoringData);
     }
 }
+
