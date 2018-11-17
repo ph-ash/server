2424d89ced873c8ade82354bcd36c281bc1acf5d
cHeeSaW <cheesaw@gmx.net>
2018-11-17 18:32:07 +0100

    add boardsubscriber

---
+++ src/EventSubscriber/BoardSubscriber.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Event\IncomingMonitoringDataEvent;
+use App\Service\Board\MonitoringDataPush;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class BoardSubscriber implements EventSubscriberInterface
+
+{
+    private $monitoringDataPush;
+
+    public function __construct(MonitoringDataPush $monitoringDataPush)
+    {
+        $this->monitoringDataPush = $monitoringDataPush;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [IncomingMonitoringDataEvent::EVENT_INCOMING_MONITORING_DATA => 'pushDataToBoard'];
+    }
+
+    public function pushDataToBoard(IncomingMonitoringDataEvent $incomingMonitoringDataEvent)
+    {
+        //TODO test
+        //TODO exception handling
+        $this->monitoringDataPush->invoke();
+    }
+}
