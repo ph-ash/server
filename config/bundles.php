5c40d4ec112354204a697d6f664b56e44b78bc3c
Christian Alexander Wolf <mail@cawolf.de>
2018-11-19 08:49:39 +0100

    added profiler and phpunit

--- config/bundles.php
+++ config/bundles.php
@@ -7,4 +7,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
