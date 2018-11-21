62e8abc594774321c8571a55a92f65dad596dbee
cHeeSaW <cheesaw@gmx.net>
2018-11-21 07:45:45 +0100

    adjust dto to fit board

--- src/Dto/MonitoringData.php
+++ src/Dto/MonitoringData.php
@@ -4,19 +4,20 @@ declare(strict_types=1);
 
 namespace App\Dto;
 
+use DateTimeImmutable;
 use Swagger\Annotations as SWG;
 
 class MonitoringData
 {
     /**
      * @var string
-     * @SWG\Property(description="The id of the Data", example="some_id")
+     * @SWG\Property(description="The id of the Data", example="monitoringData Id 1")
      */
     private $id;
 
     /**
      * @var string
-     * @SWG\Property(description="The status of the Data", example="green")
+     * @SWG\Property(description="The status of the Data", example="ok")
      */
     private $status;
 
@@ -30,7 +31,7 @@ class MonitoringData
      * @var int
      * @SWG\Property(description="The idle timeout of the data, after this time the tile turns yellow", example=60)
      */
-    private $idleTimeoutInMinutes;
+    private $idleTimeoutInSeconds;
 
     public function getId(): string
     {
@@ -47,8 +48,8 @@ class MonitoringData
         return $this->payload;
     }
 
-    public function getIdleTimeoutInMinutes(): int
+    public function getIdleTimeoutInSeconds(): int
     {
-        return $this->idleTimeoutInMinutes;
+        return $this->idleTimeoutInSeconds;
     }
 }
