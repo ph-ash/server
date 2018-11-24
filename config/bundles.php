f166f61051cea829742aff9f7cd98bc14d3ed55e
mtomala <tomala@auxmoney.com>
2018-11-24 14:32:03 +0100

    install mongodb

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
 ];
