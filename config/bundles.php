553e214ce64cd3e5814d6115cbef82fd03b91f77
cHeeSaW <cheesaw@gmx.net>
2018-11-25 19:51:07 +0100

    add symfony security bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
