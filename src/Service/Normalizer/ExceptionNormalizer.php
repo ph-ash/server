11405db4bdb666fc3765d5070a08a58d4aa0973d
cHeeSaW <cheesaw@gmx.net>
2018-11-29 22:33:56 +0100

    add exception normalizer

---
+++ src/Service/Normalizer/ExceptionNormalizer.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Normalizer;
+
+use Exception;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+class ExceptionNormalizer implements NormalizerInterface
+{
+    public function normalize($object, $format = null, array $context = array()): array
+    {
+        $templateData = $context['template_data'];
+
+        /** @var FlattenException $exception */
+        $exception = $templateData['exception'];
+        return [
+            'status' => $templateData['status'],
+            'message' => $exception->getMessage(),
+            'statusCode' => $templateData['status_code']
+
+        ];
+    }
+
+    public function supportsNormalization($data, $format = null): bool
+    {
+        return $data instanceof Exception;
+    }
+}
