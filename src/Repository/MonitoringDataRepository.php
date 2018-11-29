523a2eacdf81d2bcf427b6bcacf40113287e2812
cHeeSaW <cheesaw@gmx.net>
2018-11-29 20:00:48 +0100

    implement save method

--- src/Repository/MonitoringDataRepository.php
+++ src/Repository/MonitoringDataRepository.php
@@ -5,13 +5,28 @@ declare(strict_types=1);
 namespace App\Repository;
 
 use App\Document\MonitoringData;
+use App\Exception\PersistenceLayerException;
 use Doctrine\Bundle\MongoDBBundle\ManagerRegistry;
 use Doctrine\Bundle\MongoDBBundle\Repository\ServiceDocumentRepository;
+use Exception;
 
 class MonitoringDataRepository extends ServiceDocumentRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, MonitoringData::class);
+    }
+
+    /**
+     * @throws PersistenceLayerException
+     */
+    public function save(MonitoringData $monitoringData): void
+    {
+        try {
+            $this->getDocumentManager()->persist($monitoringData);
+            $this->getDocumentManager()->flush($monitoringData);
+        } catch (Exception $exception) {
+            throw new PersistenceLayerException('MonitoringData save failed.', 0, $exception);
+        }
     }
 }
