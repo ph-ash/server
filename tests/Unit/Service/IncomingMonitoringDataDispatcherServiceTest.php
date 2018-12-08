e4cb1e12849b6e7df89e02bdb1f9c6ec8f053d5b
cHeeSaW <cheesaw@gmx.net>
2018-12-08 20:32:09 +0100

    add test for incoming monitoring data dispatcher service

---
+++ tests/Unit/Service/IncomingMonitoringDataDispatcherServiceTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Service;
+
+use App\Dto\MonitoringData;
+use App\Event\IncomingMonitoringDataEvent as Event;
+use App\Factory\IncomingMonitoringDataEvent;
+use App\Service\IncomingMonitoringDataDispatcherService;
+use DateTimeImmutable;
+use Exception;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+class IncomingMonitoringDataDispatcherServiceTest extends TestCase
+{
+    private $eventDispatcher;
+    private $incomingMonitoringDataEvent;
+    private $subject;
+
+    /**
+     * @throws Exception
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->eventDispatcher = $this->prophesize(EventDispatcherInterface::class);
+        $this->incomingMonitoringDataEvent = $this->prophesize(IncomingMonitoringDataEvent::class);
+
+        $this->subject = new IncomingMonitoringDataDispatcherService(
+            $this->eventDispatcher->reveal(),
+            $this->incomingMonitoringDataEvent->reveal()
+        );
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testInvoke(): void
+    {
+        $monitoringData = new MonitoringData(
+            'someId',
+            'someStatus',
+            'somePayload',
+            5,
+            60,
+            new DateTimeImmutable()
+        );
+
+        $monitoringDataEvent = new Event($monitoringData);
+
+        $this->incomingMonitoringDataEvent->create($monitoringData)->willReturn($monitoringDataEvent)->shouldBeCalledOnce();
+        $this->eventDispatcher->dispatch(Event::EVENT_INCOMING_MONITORING_DATA, $monitoringDataEvent)->shouldBeCalledOnce();
+        $this->subject->invoke($monitoringData);
+    }
+}
