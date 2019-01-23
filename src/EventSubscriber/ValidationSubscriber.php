5aab53ed7821a2e503737b5fb0a9c7f42f01a64b
Martin Tomala <cheesaw@gmx.net>
2019-01-23 19:39:46 +0100

    add subscriber for validation

---
+++ src/EventSubscriber/ValidationSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Event\IncomingMonitoringDataEvent;
+use App\Service\Validation\MonitoringDataValidation;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class ValidationSubscriber implements EventSubscriberInterface
+{
+    private $monitoringDataValidation;
+
+    public function __construct(MonitoringDataValidation $monitoringDataValidation)
+    {
+        $this->monitoringDataValidation = $monitoringDataValidation;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            IncomingMonitoringDataEvent::EVENT_INCOMING_MONITORING_DATA => [
+                ['validateMonitoringData', 0]
+            ]
+        ];
+    }
+
+    public function validateMonitoringData(IncomingMonitoringDataEvent $incomingMonitoringDataEvent): void
+    {
+        $this->monitoringDataValidation->invoke($incomingMonitoringDataEvent->getMonitoringData());
+    }
+}
