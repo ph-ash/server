415e9d1d47ca089925eca51b7b04bac26c052fe8
Martin Tomala <cheesaw@gmx.net>
2019-01-24 19:12:15 +0100

    add branch/path validator

---
+++ src/Validator/Constraints/IsBranchValidator.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Validator\Constraints;
+
+use App\Exception\PersistenceLayerException;
+use App\Repository\MonitoringDataRepository;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+use Symfony\Component\Validator\Exception\UnexpectedTypeException;
+
+class IsBranchValidator extends ConstraintValidator
+{
+    private $monitoringDataRepository;
+
+    public function __construct(MonitoringDataRepository $monitoringDataRepository)
+    {
+
+        $this->monitoringDataRepository = $monitoringDataRepository;
+    }
+
+    /**
+     * @throws UnexpectedTypeException
+     * @throws PersistenceLayerException
+     */
+    public function validate($value, Constraint $constraint): void
+    {
+        if (!$constraint instanceof IsBranch) {
+            throw new UnexpectedTypeException($constraint, IsBranch::class);
+        }
+
+        $result = $this->monitoringDataRepository->findLeafs($value);
+
+        if (count($result)) {
+            $this->context->buildViolation($constraint->message)
+                ->addViolation();
+        }
+    }
+}
