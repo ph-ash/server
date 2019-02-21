6d5bb41eacaadcd262e592a97abce23518291462
Martin Tomala <cheesaw@gmx.net>
2019-02-21 10:45:57 +0100

    add throws exceptions

--- src/Service/IncomingMonitoringDataDispatcher.php
+++ src/Service/IncomingMonitoringDataDispatcher.php
@@ -6,11 +6,15 @@ namespace App\Service;
 
 use App\Dto\MonitoringData;
 use App\Exception\PersistenceLayerException;
+use OutOfBoundsException;
+use Symfony\Component\Validator\Exception\ValidatorException;
 
 interface IncomingMonitoringDataDispatcher
 {
     /**
      * @throws PersistenceLayerException
+     * @throws ValidatorException
+     * @throws OutOfBoundsException
      */
     public function invoke(MonitoringData $monitoringData): void;
 }
