5372e1ed5038e1ec3fd6953d7bd358c85ab31646
Christian Alexander Wolf <mail@cawolf.de>
2019-03-02 22:28:29 +0100

    use factory

--- src/Service/GrowTilesDispatcherService.php
+++ src/Service/GrowTilesDispatcherService.php
@@ -5,19 +5,25 @@ declare(strict_types=1);
 namespace App\Service;
 
 use App\Event\GrowTilesEvent;
+use App\Factory\GrowTilesEvent as GrowTilesEventFactory;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
 class GrowTilesDispatcherService implements GrowTilesDispatcher
 {
     private $eventDispatcher;
+    private $growTilesEventFactory;
 
-    public function __construct(EventDispatcherInterface $eventDispatcher)
+    public function __construct(EventDispatcherInterface $eventDispatcher, GrowTilesEventFactory $growTilesEventFactory)
     {
         $this->eventDispatcher = $eventDispatcher;
+        $this->growTilesEventFactory = $growTilesEventFactory;
     }
 
     public function invoke(): void
     {
-        $this->eventDispatcher->dispatch(GrowTilesEvent::EVENT_NAME);
+        $event = $this->growTilesEventFactory->create();
+        if ($event) {
+            $this->eventDispatcher->dispatch(GrowTilesEvent::EVENT_NAME, $event);
+        }
     }
 }
