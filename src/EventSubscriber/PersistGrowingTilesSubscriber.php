45570b0806b77d6657810f5f142fafb979d22397
Christian Alexander Wolf <mail@cawolf.de>
2019-03-02 22:57:14 +0100

    added persisting subscriber

---
+++ src/EventSubscriber/PersistGrowingTilesSubscriber.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventSubscriber;
+
+use App\Event\GrowTilesEvent;
+use App\Service\GrowTiles\PersistGrowingTiles;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class PersistGrowingTilesSubscriber implements EventSubscriberInterface
+{
+    private $persistGrowingTiles;
+
+    public function __construct(PersistGrowingTiles $persistGrowingTiles)
+    {
+        $this->persistGrowingTiles = $persistGrowingTiles;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            GrowTilesEvent::EVENT_NAME => ['persistGrowingTiles', -10]
+        ];
+    }
+
+    public function persistGrowingTiles(GrowTilesEvent $event): void
+    {
+        $persistedMonitorings = $this->persistGrowingTiles->invoke($event->getMonitorings());
+        if ($persistedMonitorings) {
+            $event->setMonitorings($persistedMonitorings);
+        } else {
+            $event->stopPropagation();
+        }
+    }
+}
