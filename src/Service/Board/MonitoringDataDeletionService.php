2723aab7c22c7640bb268d5a5a2459761e55a5d6
cHeeSaW <cheesaw@gmx.net>
2019-03-03 10:46:54 +0100

    add new deletion service

---
+++ src/Service/Board/MonitoringDataDeletionService.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Service\Board;
+
+use App\Dto\MonitoringData;
+
+class MonitoringDataDeletionService implements MonitoringDataPush
+{
+    public function invoke(MonitoringData $monitoringData): void
+    {
+        //TODO implement deletion
+    }
+}
