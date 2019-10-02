78de1d1b8cdce0ee9eb680901b46c505d71c1ec7
cHeeSaW <cheesaw@gmx.net>
2019-10-02 18:04:23 +0200

    remove unused function

--- src/Factory/MonitoringDataDtoFactoryService.php
+++ src/Factory/MonitoringDataDtoFactoryService.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace App\Factory;
 
 use App\Document\MonitoringData as MonitoringDataDocument;
-use App\Dto\MonitoringData as IncomingMonitoringDataDto;
 use App\Dto\Outgoing\MonitoringData as MonitoringDataDto;
 
 class MonitoringDataDtoFactoryService implements MonitoringDataDtoFactory
@@ -23,21 +22,6 @@ class MonitoringDataDtoFactoryService implements MonitoringDataDtoFactory
             $monitoringData->getTileExpansionIntervalCount(),
             $monitoringData->getTileExpansionGrowthExpression(),
             $monitoringData->getStatusChangedAt()
-        );
-    }
-
-    public function createOutgoingFromIncoming(IncomingMonitoringDataDto $monitoringData): MonitoringDataDto
-    {
-        return new MonitoringDataDto(
-            $monitoringData->getId(),
-            $monitoringData->getStatus(),
-            $monitoringData->getPayload(),
-            $monitoringData->getPriority(),
-            $monitoringData->getIdleTimeoutInSeconds(),
-            $monitoringData->getDate(),
-            $monitoringData->getPath(),
-            $monitoringData->getTileExpansionIntervalCount(),
-            $monitoringData->getTileExpansionGrowthExpression()
         );
     }
 }
