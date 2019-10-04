8af677e9f29c27ddbd09844ae523df4c9f17422f
cHeeSaW <cheesaw@gmx.net>
2019-10-04 13:52:34 +0200

    interface fix

--- src/Factory/MonitoringDataDtoFactory.php
+++ src/Factory/MonitoringDataDtoFactory.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 namespace App\Factory;
 
 use App\Document\MonitoringData as MonitoringDataDocument;
-use App\Dto\MonitoringData as IncomingMonitoringDataDto;
 use App\Dto\Outgoing\MonitoringData as OutgoingMonitoringDataDto;
 
 interface MonitoringDataDtoFactory
 {
     public function createOutgoingFromDocument(MonitoringDataDocument $monitoringData): OutgoingMonitoringDataDto;
-
-    public function createOutgoingFromIncoming(IncomingMonitoringDataDto $monitoringData): OutgoingMonitoringDataDto;
 }
